3149cfa1aec3b777b47c49ab078e0a9090b26a61
Jonathan Serafini <jonathan@lightspeedpos.com>
2015-05-29 08:38:58 -0400

    Add caching support to secure data bag items

--- lib/chef/dsl/data_query.rb
+++ lib/chef/dsl/data_query.rb
@@ -2,13 +2,20 @@
 class Chef
   module DSL
     module DataQuery
-      def secure_data_bag_item(bag, item)
-        DataBag.validate_name!(bag.to_s)
-        SecureDataBag::Item.validate_id!(item)
-        SecureDataBag::Item.load(bag, item)
-      rescue Exception
-        Log.error("Failed to load secure data bag item: #{bag.inspect} #{item.inspect}")
-        raise
+      def secure_data_bag_item(bag, item, cache: false)
+        data_bag_item = begin
+          node.run_state[:secure_data_bag][bag] || {}
+          node.run_state[:secure_bag_item][bag][item]
+        end if cache
+
+        data_bag_item ||= begin
+          DataBag.validate_name!(bag.to_s)
+          SecureDataBag::Item.validate_id!(item)
+          SecureDataBag::Item.load(bag, item)
+        rescue Exception
+          Log.error("Failed to load secure data bag item: #{bag.inspect} #{item.inspect}")
+          raise
+        end
       end
 
       def secure_data_bag_item!(item, fields=[])
